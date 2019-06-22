f0b848793187746f8ca98f8b764d7ff2ac20e073
Russell Greed <greedrussell@gmail.com>
2019-06-22 16:06:21 +0300

    feat: repair sourcemap for style and add typescript, uglify for scripts

--- gulpfile.js
+++ gulpfile.js
@@ -7,16 +7,22 @@ const handlebars = require('handlebars');
 const gulpHandlebars = require('gulp-handlebars-html')(handlebars);
 const minifyCss = require('gulp-minify-css');
 const rename = require("gulp-rename");
-const replace = require('gulp-replace')
+const replace = require('gulp-replace');
+const tinypng = require('gulp-tinypng');
 const browserSync = require('browser-sync').create();
+const browserify = require("browserify");
+const source = require('vinyl-source-stream');
+const tsify = require("tsify");
+const uglify = require('gulp-uglify');
+const buffer = require('vinyl-buffer');
 
 const style = () => {
-  return gulp.src(['./app/styles/**/*.scss'])
+  return gulp.src('./app/styles/**/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
-    .pipe(sourcemaps.write())
     .pipe(concat('style.css'))
     .pipe(minifyCss())
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('./dist/style'))
     .pipe(browserSync.stream());
 }
@@ -38,6 +44,31 @@ const templates = () => {
     .pipe(gulp.dest(`./dist`));
 }
 
+const img = () => {
+  return gulp.src(['./app/img/*.png', './app/img/*.jpg', './app/img/*.jpeg'])
+    .pipe(tinypng('vjHI7U1qQ6WhgGjqRJcY40ZmQYSzjm36'))
+    .pipe(gulp.dest('dist/img'));
+}
+
+const scripts = () => {
+  return browserify({
+    basedir: '.',
+    debug: true,
+    entries: ['app/js/main.ts'],
+    cache: {},
+    packageCache: {}
+  })
+    .plugin(tsify)
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest("dist/js"))
+    .pipe(browserSync.stream());
+}
+
 const clear = () => del(['dist/*']);
 
 const watch = () => {
@@ -47,8 +78,9 @@ const watch = () => {
 
   gulp.watch('./app/*.hbs', templates).on('all', browserSync.reload);
   gulp.watch('./app/styles/*.scss', style);
+  gulp.watch('./app/js/*.ts', scripts);
 }
 
 gulp.task('clear', clear);
-gulp.task('build', gulp.series(clear, style, templates));
+gulp.task('build', gulp.series(clear, style, templates, scripts));
 gulp.task('watch', gulp.series(['build'] ,watch));
